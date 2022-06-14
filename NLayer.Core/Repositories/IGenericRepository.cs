109f99dce4693b62b9df56030534bfd9956c3b96
Fuat Aydin <81709694+fuataydinn@users.noreply.github.com>
2022-06-15 00:05:28 +0300

    created project
    15/06/2022

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        //productRepository.GetAll(x=>x.id>5).ToList();
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        Task<T> GetByIdAsync(int id);
+        //Burada direk veri tabanına sorgu yapmadıgımız icin asenkron degil
+
+        //productRepository.where(x=>x.id>5).ToListAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression); 
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities); //Soyut nesnelerle calısmaya calıs genericlerde
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression); //Var mı ? Yok mu ? 
+        void Update(T entity); //Update ve remove asenkron metod degıl, bunlarda ef'de sadece state'i degistigi icin 
+        //Database'i yoran islemler degildir.
+        void Delete(T entity);   
+        void RemoveRange(IEnumerable<T> entities);
+
+    }
+}
---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)  //Basedeki options'a yolluyoruz
+        {
+        //Bu optionsla beraber veri tabanı yolunu startup'ta vermek icin bu constructor'ı olusturmamız gerekiyordu
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Tum configuration dosyalarını reflextion ile burada tek seferde uyguluyoruz.
+            //Bunu da IEntityTypeConfiguration<Category> seklinde kalıtım alan sınıfları bularak yapıyor
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Entity<ProductFeature>().HasData(
+                new ProductFeature() { Id = 1, Color = "Kırmızı", Height = 100, Width = 200,ProductId=1 },
+                new ProductFeature() { Id = 2, Color = "Mavi", Height = 300, Width = 200,ProductId=2 }
+                      
+                );  //istersek buradan da seed data olusturabiliriz'i gormek icin bunu yaptık
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
